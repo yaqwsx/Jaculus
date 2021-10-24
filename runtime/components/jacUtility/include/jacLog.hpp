ee31e8e09b04012d4545d2c775a670bce99db26d
Jaroslav Malec <dzardacz@gmail.com>
2021-10-24 22:16:26 +0200

    Add remaining log levels

--- runtime/components/jacUtility/include/jacLog.hpp
+++ runtime/components/jacUtility/include/jacLog.hpp
@@ -2,5 +2,8 @@
 
 #include <esp_log.h>
 
-#define JAC_LOGI( tag, format, ... ) ESP_LOGI( tag, format, ##__VA_ARGS__ )
+#define JAC_LOGE( tag, format, ... ) ESP_LOGE( tag, format, ##__VA_ARGS__ )
 #define JAC_LOGW( tag, format, ... ) ESP_LOGW( tag, format, ##__VA_ARGS__ )
+#define JAC_LOGI( tag, format, ... ) ESP_LOGI( tag, format, ##__VA_ARGS__ )
+#define JAC_LOGD( tag, format, ... ) ESP_LOGD( tag, format, ##__VA_ARGS__ )
+#define JAC_LOGV( tag, format, ... ) ESP_LOGV( tag, format, ##__VA_ARGS__ )
